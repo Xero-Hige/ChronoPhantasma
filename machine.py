87d6921ddaeb2e7da5327892bc9ae1efdc1f3de6
Xero-Hige <gaston.martinez.90@gmail.com>
2017-11-22 13:24:30 -0300

    Updated machine to run as process

--- machine.py
+++ machine.py
@@ -3,22 +3,23 @@ from time import sleep
 
 
 class Machine(multiprocessing.Process):
-    def __init__(self, machine_queue, job_time):
+    def __init__(self, machine_queue, client_queues, job_time):
         multiprocessing.Process.__init__(self)
         self.exit = multiprocessing.Event()
 
         self.machine_queue = machine_queue
+        self.client_queues = client_queues
         self.job_time = job_time
 
     def do_job(self, job_size):
         sleep(self.job_time * job_size)
 
-    def do_process(self):
+    def run(self):
         job = self.machine_queue.get()
         while job:
-            queue, job_size = job
+            client_number, job_size = job
             self.do_job(job_size)
-            queue.put(0)
+            self.client_queues[client_number].put(0)
             job = self.machine_queue.get()
 
     def shutdown(self):
